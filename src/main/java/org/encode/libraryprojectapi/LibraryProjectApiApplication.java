3593d2358f1f6bf412d95bd046dfd5757dc3a899
Karagwa <annkaragwa@gmail.com>
2024-08-12 16:09:57 +0300

    Changes in the main application class

--- src/main/java/org/encode/libraryprojectapi/LibraryProjectApiApplication.java
+++ src/main/java/org/encode/libraryprojectapi/LibraryProjectApiApplication.java
@@ -2,6 +2,10 @@ package org.encode.libraryprojectapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class LibraryProjectApiApplication {
@@ -9,5 +13,7 @@ public class LibraryProjectApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryProjectApiApplication.class, args);
 	}
+
+
 
 }
