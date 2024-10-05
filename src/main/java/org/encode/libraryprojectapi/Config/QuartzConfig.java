b0d66c0ec61efcff8079c1f07d282a8ff71a2c6b
Karagwa <annkaragwa@gmail.com>
2024-10-05 16:19:48 +0300

    Added the quartz and velocity configuration classes

--- src/main/java/org/encode/libraryprojectapi/Config/QuartzConfig.java
+++ src/main/java/org/encode/libraryprojectapi/Config/QuartzConfig.java
@@ -1,2 +1,13 @@
-package org.encode.libraryprojectapi.Config;public class QuartzConfig {
-}
+//package org.encode.libraryprojectapi.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+//
+//@Configuration
+//public class QuartzConfig {
+//    @Bean
+//    public SchedulerFactoryBean quartzScheduler(){
+//
+//    }
+//}
--- src/main/java/org/encode/libraryprojectapi/Config/VelocityConfig.java
+++ src/main/java/org/encode/libraryprojectapi/Config/VelocityConfig.java
@@ -1,2 +1,26 @@
-package org.encode.libraryprojectapi.Config;public class VelocityConfig {
+package org.encode.libraryprojectapi.Config;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class VelocityConfig {
+
+    @Bean
+    public VelocityEngine velocityEngine() {
+        VelocityEngine velocityEngine = new VelocityEngine();
+        Properties properties = new Properties();
+
+        properties.setProperty("resource.loader", "class");
+        properties.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        properties.setProperty("velocity.template.root", "templates/");
+
+        velocityEngine.init(properties);
+
+        return velocityEngine;
+    }
 }
+
